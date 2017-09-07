a1be386b53166b31b250fbbff19b10d395883dae
digitopoi <hubo85@gmail.com>
2017-09-07 11:39:39 -0500

    Change date formatting

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -5,7 +5,8 @@ namespace MvcMovie.Models {
     public class Movie {
         public int ID { get; set; }
         public string Title { get; set; }
-        [DisplayFormat(DataFormatString = "{0:MM-dd-yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)] 
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
