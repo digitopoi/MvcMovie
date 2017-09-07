f00f475ec5ee78fde35c8feb345428e9a2ea0333
digitopoi <hubo85@gmail.com>
2017-09-06 20:08:32 -0500

    Modify Welcome controller

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -1,23 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Encodings.Web;
 
-namespace MvcMovie.Controllers 
-{
-    public class HelloWorldController: Controller
-    {
+namespace MvcMovie.Controllers {
+    public class HelloWorldController: Controller{
         //  
         //  GET: /HelloWorld/
-        public string Index() 
-        {
+        public string Index() {
             return "This is my default action...";
         }
 
         //  
         //  GET: /HelloWorld/Welcome/
-
-        public string Welcome()
-        {
-            return "This is the Welcome action method...";
+        public string Welcome(string name, int ID = 1) {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID is {ID}");
         }
     }
 }
